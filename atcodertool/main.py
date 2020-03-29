f1187ac9567c638984b7d5eef549d599735c1034
sagoj0_ <nisi3genki@gmail.com>
2020-03-29 23:17:18 +0900

    help時の説明を追加

--- atcodertool/main.py
+++ atcodertool/main.py
@@ -9,7 +9,7 @@ import atcodertool.config as conf
 
 
 class RootCommand(Command):
-    name = "atcoder-tool"
+    name = "atcoder_tool"
     long_description = "this is helper for testing and submitting on atcoder"
 
     def build_option(self, parser):
@@ -46,10 +46,10 @@ class Run(Command):
 
 class Test(Command):
     name = "test"
-    short_description = "testing codes. USAGE: atcoder-tool test c"
+    short_description = "testing codes."
 
     def build_option(self, parser):
-        parser.add_argument("problem_id")
+        parser.add_argument("problem_id", help="problem name (e.g. a)")
         return parser
 
     def run(self, args):
@@ -59,8 +59,7 @@ class Test(Command):
 
 class New(Command):
     name = "new"
-    short_description = "create new contest. USAGE: atcoder-tool new abcxxx"
-    long_description = "create new contest"
+    short_description = "create new contest."
 
     def build_option(self, parser):
         parser.add_argument("contest_id",help="contest name (e.g. abc042)")
@@ -84,7 +83,7 @@ class New(Command):
 
 class Login(Command):
     name = "login"
-    short_description = "login to atcoder."
+    short_description = "log in to atcoder."
 
     def run(self, args):
         if os.path.exists(com.COOKIE_FILE):
@@ -95,9 +94,10 @@ class Login(Command):
 
 class Submit(Command):
     name = "submit"
+    short_description = "submit source codes to atcoder."
 
     def build_option(self, parser):
-        parser.add_argument("problem_id")
+        parser.add_argument("problem_id", help="problem name (e.g. a)")
         parser.add_argument("-f", "--force", action="store_true")
         return parser
 
@@ -127,6 +127,7 @@ def submitting(problem_id, testing):
 
 class Logout(Command):
     name = "logout"
+    short_description = "log out from atcoder."
     def run(self, args):
         os.remove(com.COOKIE_FILE)
         print("you logged out from atcoder.")
