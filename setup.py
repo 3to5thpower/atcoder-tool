0454d8878b449e9c0d1acae38c77c766642be5a6
sagoj0_ <nisi3genki@gmail.com>
2020-03-29 21:06:49 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name="atcoder_tool",
+    version="0.1",
     install_requires=["requests", "beautifulsoup4", "uroboros", "toml", "lxml"],
     entry_points={
         "console_scripts":[
