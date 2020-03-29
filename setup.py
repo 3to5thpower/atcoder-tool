545e6550145351a616f8dfcbc8e4fe12b9c9d658
sagoj0_ <nisi3genki@gmail.com>
2020-03-29 22:02:01 +0900

    replace package name _ to -

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="atcoder_tool",
+    name="atcoder-tool",
     version="0.1",
     install_requires=["requests", "beautifulsoup4", "uroboros", "toml", "lxml"],
     entry_points={
         "console_scripts":[
-            "atcoder_tool = atcoder_tool.atcoder_tool:main"
+            "atcoder_tool = atcoder-tool.atcoder-tool:main"
         ]
     }
 )
